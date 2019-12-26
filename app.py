e6c2a2f73e611c3df0a354ccba5d98ba3b10f306
mister0825 <30447239+mister0825@users.noreply.github.com>
2019-12-25 19:07:09 -0800

    Refactor Flask app to properly render random user data.

--- app.py
+++ app.py
@@ -9,16 +9,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	try:
-		# Generate and add accounts to DB
+		# Create data store
+		ds = []
+
+		# Generate and add entries to data store
 		for i in range(0, 10):
 			full_name = names.get_full_name()
 			first_name = full_name.split()[0]
 			last_name = full_name.split()[1]
 			uid = full_name.lower().replace(" ", ".")
 
-		# Select all account records
-		result = (uid, last_name, first_name)
+		# Create tuple with user elements
+			user = (uid, first_name, last_name)
 
+		# Append entry elements to data store
+			ds.append(user)
+
+		# Return all data store elements
+		result = (ds[0:])
 		return render_template('userlist.html', result=result)
 		
 	except Exception as e:
